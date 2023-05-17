c62bfdc49c59f2282a022539ade56be084b020a1
Swiss <69964126+ogechibogu@users.noreply.github.com>
2023-05-17 17:51:49 +0200

    Update update.sh

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -1,3 +1,5 @@
+echo "lets do this shiiii"
+:'
 CHARTS=$(ls -d */ | sed 's|/||')
 for C in $CHARTS;do
    echo "checking $C for updates" 
@@ -56,3 +58,4 @@ for C in $CHARTS;do
         echo "$C updated successfully"
      fi
  done
+ '
